422269bddccba6f53b534aabce934340adb744bf
cong <congyunlong@shequcun.com>
2016-03-05 11:59:27 +0800

    upload project

---
+++ src/com/cyl/xml/Dimen.java
@@ -0,0 +1,11 @@
+package com.cyl.xml;
+
+public class Dimen {
+	String name;
+	String value;
+
+	@Override
+	public String toString() {
+		return "Dimen [name=" + name + ", value=" + value + "]";
+	}
+}
---
+++ src/com/cyl/xml/Test.java
@@ -0,0 +1,8 @@
+package com.cyl.xml;
+
+public class Test {
+	public static void main(String[] args) {
+		XmlHandler handle = new StringXmlHandler();
+		handle.handle("/Users/cong/myxml/assets");
+	}
+}
